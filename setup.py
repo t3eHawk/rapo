3b81688e2ba3b5aa22f0c40965e3b5f664ecea11
t3eHawk <timurfaradzhov@gmail.com>
2020-10-14 17:48:45 +0300

    Update setup.py for web API development

--- setup.py
+++ setup.py
@@ -1,34 +1,36 @@
-"""Setup of RAPO."""
+"""Setup script for RAPO package."""
 
 import rapo
 import setuptools
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
 
-install_requires = ['sqlalchemy', 'cx_oracle', 'pepperoni']
-
-author = rapo.__author__
-email = rapo.__email__
+name = 'rapo'
 version = rapo.__version__
+author = rapo.__author__
+author_email = rapo.__email__
 description = rapo.__doc__
+long_description = open('README.md', 'r').read()
+long_description_content_type = 'text/markdown'
 license = rapo.__license__
+url = 'https://github.com/t3eHawk/rapo'
+install_requires = ['pepperoni', 'psutil',
+                    'sqlalchemy', 'cx_oracle',
+                    'flask', 'flask_httpauth', 'waitress']
+packages = setuptools.find_packages()
+classifiers = ['Programming Language :: Python :: 3',
+               'License :: OSI Approved :: MIT License',
+               'Operating System :: OS Independent']
 
-setuptools.setup(
-    name='rapo',
-    version=version,
-    author=author,
-    author_email=email,
-    description=description,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    license=license,
-    url='https://github.com/t3eHawk/rapo',
-    install_requires=install_requires,
-    packages=setuptools.find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-)
+
+setuptools.setup(name=name,
+                 version=version,
+                 author=author,
+                 author_email=author_email,
+                 description=description,
+                 long_description=long_description,
+                 long_description_content_type=long_description_content_type,
+                 license=license,
+                 url=url,
+                 install_requires=install_requires,
+                 packages=packages,
+                 classifiers=classifiers)

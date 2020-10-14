43c8f66389d654801a9561f69e39403bce27aa26
t3eHawk <timurfaradzhov@gmail.com>
2020-10-14 17:53:10 +0300

    Add attribute with app tables into database interface

--- rapo/database.py
+++ rapo/database.py
@@ -2,7 +2,7 @@
 
 import sys
 
-import sqlalchemy as sql
+import sqlalchemy as sa
 
 from .config import config
 
@@ -13,27 +13,53 @@ class Database():
     ckwargs = {'literal_binds': True}
 
     def __init__(self):
-        if config.has_section('DATABASE') is True:
-            vendor = config['DATABASE']['vendor']
-            host = config['DATABASE']['host']
-            port = config['DATABASE']['port']
-            path = config['DATABASE'].get('path')
-            sid = config['DATABASE'].get('sid')
-            service = config['DATABASE'].get('service')
-            user = config['DATABASE']['user']
-            password = config['DATABASE']['password']
-            if vendor == 'sqlite' and path is not None:
-                if sys.platform.startswith('win'):
-                    url = f'{vendor}:///{path}'
-                else:
-                    url = f'{vendor}:////{path}'
+        vendor = config['DATABASE']['vendor']
+        host = config['DATABASE']['host']
+        port = config['DATABASE']['port']
+        path = config['DATABASE'].get('path')
+        sid = config['DATABASE'].get('sid')
+        service = config['DATABASE'].get('service')
+        user = config['DATABASE']['user']
+        password = config['DATABASE']['password']
+        if vendor == 'sqlite' and path is not None:
+            if sys.platform.startswith('win'):
+                url = f'{vendor}:///{path}'
             else:
-                url = f'{vendor}://{user}:{password}@{host}:{port}'
-                if sid is not None:
-                    url += f'/{sid}'
-                elif service is not None:
-                    url += f'/?service_name={service}'
-            self.engine = sql.create_engine(url)
+                url = f'{vendor}:////{path}'
+        else:
+            url = f'{vendor}://{user}:{password}@{host}:{port}'
+            if sid is not None:
+                url += f'/{sid}'
+            elif service is not None:
+                url += f'/?service_name={service}'
+        settings = dict(pool_pre_ping=True, max_identifier_length=128)
+        self.engine = sa.create_engine(url, **settings)
+        self.tables = self.Tables(database=self)
+        pass
+
+    class Tables():
+        """Represents database tables."""
+
+        def __init__(self, database=None):
+            self.database = database
+            self.config = self.load('rapo_config')
+            self.log = self.load('rapo_log')
+            self.scheduler = self.load('rapo_scheduler')
+            pass
+
+        def load(self, name):
+            """Load one of database table by name.
+
+            Returns
+            -------
+            table : sqlalchemy.Table
+                Database table instance.
+            """
+            meta = sa.MetaData()
+            engine = self.database.engine
+            table = sa.Table(name, meta, autoload=True, autoload_with=engine)
+            return table
+
         pass
 
     def connect(self):
@@ -55,8 +81,11 @@ class Database():
         table : sqlalchemy.Table
             Database table instance.
         """
-        meta = sql.MetaData()
-        return sql.Table(name, meta, autoload=True, autoload_with=db.engine)
+        meta = sa.MetaData()
+        table = sa.Table(name, meta, autoload=True, autoload_with=self.engine)
+        return table
+
+    pass
 
 
 db = Database()

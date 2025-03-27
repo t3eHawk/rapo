22a44cbad799789a0404e33e43f543005f9a4a8e
t3eHawk <timurfaradzhov@gmail.com>
2025-03-27 14:40:24 +0300

    Set version 0.6.2

--- rapo/__init__.py
+++ rapo/__init__.py
@@ -10,7 +10,7 @@ __copyright__ = 'Copyright 2024, The Rapo project'
 __credits__ = ['Timur Faradzhov', 'Kostadin Taneski']
 
 __license__ = 'MIT'
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 __maintainer__ = 'Timur Faradzhov'
 __email__ = 'timurfaradzhov@gmail.com'
 __status__ = 'Development'

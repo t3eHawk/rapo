115df55b7fa562439ac2a77255c8cd9918d3ff32
Kostadin Taneski <kosta@taneski.com>
2026-01-29 11:39:01 +0100

    Fix the bug causing false corelation of clustered records

--- rapo/algorithms/reconciliation/s03_prepare_duplicates_a.sql
+++ rapo/algorithms/reconciliation/s03_prepare_duplicates_a.sql
@@ -1,6 +1,6 @@
 create table rapo_temp_t03_dup_a_{process_id} as
 select {parallelism} a.*, x.cluster_id,
-       row_number() over (partition by {keys_a}, x.cluster_id order by o.numeric_value, {key_field_a}) as cluster_position_number
+       row_number() over (partition by {key_field_a}, x.cluster_id order by o.numeric_value, {keys_a}) as cluster_position_number
   from rapo_temp_source_a_{process_id} a
        left join rapo_temp_t02_org_a_{process_id} o on {key_field_a} = o.a_id
        left join (select a_id, listagg(b_id, '~' on overflow truncate) within group (order by b_id) as b_group_id, cluster_id from rapo_temp_t01_mod_{process_id} group by a_id, cluster_id) x
